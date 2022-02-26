6b7772e244b8d3ca632569331298fc76e515c00a
Matt Williams <lpugfan2004@gmail.com>
2022-02-26 17:44:36 -0500

    bc it cant be empty..

--- config/Database.php
+++ config/Database.php
@@ -3,7 +3,7 @@
     // DB Params
     private $host = 'xlf3ljx3beaucz9x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
     private $db_name = 'ed4m4w1lz2o28h48';
-    private $username = getenv('JAWSDB_USER');
+    private $username = 'c45yfcitsb3w40ak';
     private $password = getenv('JAWSDB_PASS');
     private $conn;
 
@@ -16,6 +16,7 @@
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       } catch(PDOException $e) {
         echo 'Connection Error: ' . $e->getMessage();
+        echo 'Connection does not work';
       }
 
       return $this->conn;
